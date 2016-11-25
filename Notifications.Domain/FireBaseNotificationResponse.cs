8c57aa0c8bf524ae8282b4a487a94e28533f0a0a
Richard Lindeberg <richard.lindeberg@qliro.com>
2016-11-25 17:23:19 +0100

    NotificationSender started

--- Notifications.Domain/FireBaseNotificationResponse.cs
+++ Notifications.Domain/FireBaseNotificationResponse.cs
@@ -2,12 +2,15 @@ namespace Notifications.Domain
 {
     public class FireBaseNotificationResponse
     {
-        public FireBaseNotificationResponse(bool wasSent, bool shouldBeDeleted, string errorMessage)
+        public FireBaseNotificationResponse(bool wasSent, bool shouldBeDeleted, string errorMessage, string firebaseMessageId)
         {
             WasSent = wasSent;
             ShouldBeDeleted = shouldBeDeleted;
             ErrorMessage = errorMessage;
+            FirebaseMessageId = firebaseMessageId;
         }
+
+        public string FirebaseMessageId { get; }
 
         public bool WasSent { get; }
 
